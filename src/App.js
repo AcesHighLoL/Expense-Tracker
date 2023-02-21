bad97cd9dd5b881b3d95008f48865b25648d40e2
George Guardia <georgeguardia01@gmail.com>
2023-02-21 03:46:10 -0500

    UI done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import {Header} from './components/Header'
-import {Balance} from './components/Balance'
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpense } from './components/IncomeExpense'
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div className='container'>
-        <Balance/>
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
